3d91d37dce45588c96dcbeec44e3926ce23e71f2
Hanna Adenholm <hanna.adenholm@se.experis.com>
2025-09-05 11:51:08 +0200

    Finished the core

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   newFriend: string | null = null;
-  favorite: string | null = null;
+  favourite: string | null = null;
 
   people: string[] = ['lewis', 'jules', 'ed', 'nathan', 'dave', 'nigel'];
 
@@ -17,5 +17,9 @@ export class AppComponent {
     }
     this.people.push(this.newFriend);
     this.newFriend = null;
+  }
+
+  setFavourite(person: string) {
+    this.favourite = person;
   }
 }
